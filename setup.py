6fa059df76608fe127259086811ee104426970cb
Chinni Chaitanya <chchaitanya95@gmail.com>
2020-06-18 10:36:29 +0530

    :wrench: Update package name to avoid conflicts

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", mode="r") as fd:
     long_description = fd.read()
 
 setuptools.setup(
-    name="slack-logger",
+    name="python-slack-logger",
     version="0.9.0",
     author="Chaitanya Chinni",
     description="Slack Logger is a custom message logger to Slack for Python 3+",
